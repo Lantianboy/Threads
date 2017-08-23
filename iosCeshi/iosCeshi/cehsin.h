b71d9abfe21f9c56a827f0953b6fb18e62ae7cb5
Lantianboy <1044722126@qq.com>
2017-08-23 15:34:46 +0800

    初始化

---
+++ iosCeshi/iosCeshi/cehsin.h
@@ -0,0 +1,13 @@
+//
+//  cehsin.h
+//  iosCeshi
+//
+//  Created by 最爱是深蓝 on 2017/8/23.
+//  Copyright © 2017年 ProductTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cehsin : UIView
+
+@end
---
+++ iosCeshi/iosCeshi/cehsin.m
@@ -0,0 +1,28 @@
+//
+//  cehsin.m
+//  iosCeshi
+//
+//  Created by 最爱是深蓝 on 2017/8/23.
+//  Copyright © 2017年 ProductTest. All rights reserved.
+//
+
+#import "cehsin.h"
+
+@implementation cehsin
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame] ;
+    return self ;
+}
+
+@end
